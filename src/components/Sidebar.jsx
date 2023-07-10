e5d688a0b75302b4cb9b0d20d36a2601e8b91d3a
Larss30 <larencearroz@gmail.com>
2023-07-10 22:41:11 +0800

    videos

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import { Stack } from '@mui/material'
 import { categories } from './utils/constants'
 
-const selectedCategory = 'New'
-
-const Sidebar = () => {
+const Sidebar = ({selectedCategory, setSelectedCategory}) => {
   return (
     <Stack
     direction='row'
@@ -16,9 +14,13 @@ const Sidebar = () => {
     >
         {
             categories.map((category) => (
-                <button className='category-btn' style={{background: category.name === selectedCategory && '#fc1503', color:'#fff'}}>
-                    <span>{category.icon}</span>
-                    <span>{category.name}</span>
+                <button 
+                    className='category-btn' 
+                    onClick={() => setSelectedCategory(category.name)}
+                    style={{background: category.name === selectedCategory && '#fc1503', color:'#fff'}}
+                    key={category.name}>
+                      <span style={{color: category.name === selectedCategory ? '#fff' : '#fc1503', marginRight:'15px'}}>{category.icon}</span>
+                      <span style={{opacity: category.name === selectedCategory ? '1' : '0.8'}}>{category.name }</span>
                 </button>
         ))}
     </Stack>
---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+import React from 'react'
+
+const VideoCard = () => {
+  return (
+    <div>
+        VideoCard
+    </div>
+  )
+}
+
+export default VideoCard
