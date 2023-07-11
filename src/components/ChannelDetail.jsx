0aa1e0957d8c3808fcecd3d4b1ce4e3d284d5c15
Larss30 <larencearroz@gmail.com>
2023-07-11 20:11:16 +0800

    channels page update

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import {Videos, ChannelCard} from './'
+import { fetchFromAPI } from './utils/fetchFromAPI'
 
 const ChannelDetail = () => {
+  const { id } = useParams();
+
+  const [ChannelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+
+  console.log(ChannelDetail)
+  console.log(videos)
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part="snippet&id=${id}`).then((data) => setChannelDetail(data?.items[0]))
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&id&order=date`).then((data) => setVideos(data?.items))
+  }, [id])
+  
+
   return (
-    <div>
-      ChannelDetail
-    </div>
-  )
-}
+    <Box minHeight='95vh'>
+      <Box>
+        <div 
+        style={{background: 'linear-gradient(90deg, rgba(77,73,134,1) 0%, rgba(34,34,181,1) 24%, rgba(29,102,117,1) 100%)', zIndex: 10, height: '300px'}}/>
+        <ChannelCard channelDetail={ChannelDetail} marginTop='-100px'/>
+      </Box>
+      <Box display='flex' p='2'>
+        <Videos videos={videos} justifyContent='center'/>
+      </Box>
+    </Box>
+    )
+  }
 
 export default ChannelDetail
--- src/components/utils/constants.js
+++ src/components/utils/constants.js
@@ -11,13 +11,15 @@ import GraphicEqIcon from '@mui/icons-material/GraphicEq';
 import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+import AlbumIcon from '@mui/icons-material/Album';
 
 export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  { name: 'JS Mastery', icon: <CodeIcon />, },
-  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'Kanye West', icon: <AlbumIcon />, },
+  { name: 'Arsenal', icon: <SportsSoccerIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
   { name: 'Music', icon: <MusicNoteIcon /> },
